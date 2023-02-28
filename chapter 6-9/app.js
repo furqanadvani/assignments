f497cc98ba75c694146833356df9714c7ed7bcf0
furqanadvani <123248423+furqanadvani@users.noreply.github.com>
2023-02-28 11:36:14 -0800

    Add files via upload

---
+++ chapter 6-9/app.js
@@ -0,0 +1,322 @@
+
+
+// Q1
+// var airthmeticFunction = prompt("any number")
+// document.write(` Result: <br> the value of a is ${airthmeticFunction} <br> ------------------------- <br>
+//  The value of ++a is ${airthmeticFunction} <br> Now thw value of a is: ${Number(++airthmeticFunction)}
+//  <br>
+//  The value of a++ is ${airthmeticFunction} <br> Now thw value of a is: ${Number(airthmeticFunction++)}
+//  <br>
+//  The value of --a is ${airthmeticFunction} <br> Now thw value of a is: ${Number(--airthmeticFunction)}
+//  <br>
+//  The value of a-- is ${airthmeticFunction} <br> Now thw value of a is: ${Number(airthmeticFunction--)}`)
+// var a = prompt(""), b = prompt("");
+//  var result = --a - --b + ++b + b--;
+//  document.write(result)
+// document.write(`--a = ${Number(--a)} <br> --a - --b = ${Number(--a - --b)} <br>
+//  --a - --b + ++b = ${Number(--a - --b + ++b)} <br> --a - --b + ++b + b-- = ${Number(--a - --b + ++b + b--)}`)
+
+// // var clientName = prompt("enter your name")
+// document.write(`${clientName} welcome to our website hope you will get what you want`)
+
+// var totalMarks = "100"
+// var english = prompt(" enter obtained marks")
+// var urdu = prompt("enter obtained marks")
+// var math = prompt("enter obtained marks")
+// document.write(`<h1>Total marks  Obtained marks  percentage</h1>`)
+// document.write(`English ${totalMarks} ${english} ${Number(english)/ Number(totalMarks)*100}  <br> urdu ${totalMarks} ${urdu}
+//  ${Number(urdu)/ Number(totalMarks)*100} <br> Math ${totalMarks} ${math} ${Number(math)/ Number(totalMarks)*100}`)
+
+// var cityName = prompt("Enter your city")
+// if(cityName == "karachi"){
+//     document.write("Welcome to the city of lights")
+// }else{
+//     document.write("Welcome to the " + cityName)
+
+// }
+
+// var userInput = prompt("Enter your gender")
+// if(userInput == "male"){
+//     document.write("Good morning sir")
+// }else if( userInput == "female"){
+//     document.write("Good morning Ma'am")
+
+// }
+
+// var traficSignal = prompt("Enter Traffic Signal color")
+// if(traficSignal == "red"){
+//     document.write("Must stop")
+
+// }else if(traficSignal == "yellow"){
+//     document.write("Ready to move")
+
+// }else if(traficSignal == "green"){
+//     document.write("move now")
+
+// }
+// var remainingFuel = prompt("your remaining fuel")
+// if(remainingFuel < 0.25){
+//     document.write("Please refill your fuel")
+
+// }else{
+//     document.write("enjoy your ride")
+
+// }
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }else{
+// alert("given condition for variable b is false");
+
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// var materialCost = 100000;
+// var laborCost = 10000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost <= laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+// var phara = prompt("enter any table")
+// if(phara){
+//     document.write(`5 x 1 = ${phara} <br>
+//                 5 x 2 = ${phara} <br>
+//                 5 x 3 = ${phara} <br>
+//                 5 x 4 = ${phara} <br>
+//                 5 x 5 = ${phara} <br>
+//                 5 x 6 = ${phara} <br>
+//                 5 x 7 = ${phara} <br>
+//                 5 x 8 = ${phara} <br>
+//                 5 x 9 = ${phara} <br>
+//                 5 x 10 = ${phara} <br>`)
+
+// }else{
+// document.write(`5 x 1 = 5 <br>
+//                 5 x 2 = 10 <br>
+//                 5 x 3 = 15 <br>
+//                 5 x 4 = 20 <br>
+//                 5 x 5 = 25 <br>
+//                 5 x 6 = 30 <br>
+//                 5 x 7 = 35 <br>
+//                 5 x 8 = 40 <br>
+//                 5 x 9 = 45 <br>
+//                 5 x 10 = 50 <br>`)
+// }
+
+// var totalNumber = "300"
+// var English = prompt("enter your marks")
+// var Islamiat = prompt("enter your marks")
+// var chemistry = prompt("enter your marks")
+// var pernct = Number(English)+ Number(Islamiat)+ Number(chemistry)*100 /300
+// if(pernct > 80){
+//     document.write(`Total marks: 300 <br> Obtained marks ${Number(English)+ Number(Islamiat)+ Number(chemistry)}
+//     <br> Percentage ${pernct}<br> Grade: A-one <br> Remarks: Excellent`)
+
+// }else if(pernct> 70){
+//     document.write(`Total marks: 300 <br> Obtained marks ${Number(English)+ Number(Islamiat)+ Number(chemistry)}
+//     <br> Percentage ${pernct}<br> Grade: A <br> Remarks: Good`)
+    
+// }else if(pernct > 60){
+//     document.write(`Total marks: 300 <br> Obtained marks ${Number(English)+ Number(Islamiat)+ Number(chemistry)}
+//     <br> Percentage ${pernct}<br> Grade: B <br> Remarks: you need to improvement`)
+
+// }else if(pernct < 60){
+//     document.write(`Total marks: 300 <br> Obtained marks ${Number(English)+ Number(Islamiat)+ Number(chemistry)}
+//     <br> Percentage ${pernct}<br> Grade: fail <br> Remarks: sorry`)
+
+// }
+
+// var number$ = prompt("any number")
+// if(number$ >= 3){
+//     document.write(`your number is divisible by 3`)
+
+// }else{
+//     document.write(`your number is not divisible by 3`)
+
+// }
+
+// const evennum = prompt("enter a number")
+
+// if (evennum % 2 == 0 ){
+//     document.write("even number")
+// }else{
+//     document.write("odd number")
+
+// }
+
+//  var temp = prompt("enter eny temprature")
+//  if(temp > 40){
+//     document.write("It is too hot outside.")
+//  }else if(temp > 30){
+//     document.write("It is too hot outside.")
+    
+//  }else if(temp > 20){
+//     document.write("Today’s Weather is cool.")
+
+//  }else if(temp > 10){
+//     document.write("OMG! Today’s weather is so Cool.")
+
+//  }
+
+// var caracter = prompt("any letter")
+
+// if(caracter == caracter.toUpperCase()){
+//     document.write("uppercase")
+
+// }else if(caracter == caracter.toLowerCase()){
+//     document.write("lowercase")
+
+// }
+
+// var integer = prompt("any number")
+// var intege2 = prompt("any number")
+// if(integer > intege2){
+//     document.write(`${integer} is larger then ${intege2}`)
+// }else if(intege2 > integer){
+//     document.write(`${intege2} is larger then ${integer }`)
+
+// }else {
+//     document.write(`${integer} is equal ${intege2}`)
+
+// }
+
+// var inputnum = prompt("any number")
+// if(inputnum > 0){
+//     document.write(`number is in positive`)
+// }else if(inputnum < 0){
+//     document.write(`number is in negitive`)
+// }else if(inputnum == 0){
+// document.write(`number is in 0`)
+// }
+
+// var passcode = "furqan777"
+// var passcode2 = prompt("")
+// if(passcode == passcode2){
+//     document.write("correct!")
+// }else{
+//     document.write("plese ENTER correct!")
+
+// }
+
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {   
+// greeting = "Good day";
+// alert(greeting)
+// }else{
+// greeting = "Good evening";
+// alert(greeting)
+// }
+ 
+// var time = prompt("enter a time")
+// if(time == "100"){
+//     document.write("time is 1:00Am")
+
+// }else if(time == "200"){
+//     document.write("time is 2:00Am")
+
+// }else if(time == "300"){
+//     document.write("time is 3:00Am")
+
+// }else if(time == "400"){
+//     document.write("time is 4:00Am")
+
+// }else if(time == "500"){
+//     document.write("time is 5:00Am")
+
+// }else if(time == "600"){
+//     document.write("time is 6:00Am")
+
+// }else if(time == "700"){
+//     document.write("time is 7:00Am")
+
+// }else if(time == "800"){
+//     document.write("time is 8:00Am")
+
+// }else if(time == "900"){
+//     document.write("time is 9:00Am")
+
+// }else if(time == "1000"){
+//     document.write("time is 10:00Am")
+
+// }else if(time == "1100"){
+//     document.write("time is 11:00Am")
+
+// }else if(time == "1200"){
+//     document.write("time is 12:00Pm")
+
+// }else if(time == "1300"){
+//     document.write("time is 1:00Pm")
+
+// }else if(time == "1400"){
+//     document.write("time is 2:00Pm")
+
+// }else if(time == "1500"){
+//     document.write("time is 3:00Pm")
+
+// }else if(time == "1600"){
+//     document.write("time is 4:00Pm")
+
+// }else if(time == "1700"){
+//     document.write("time is 5:00Pm")
+
+// }else if(time == "1800"){
+//     document.write("time is 6:00Pm")
+
+// }else if(time == "1900"){
+//     document.write("time is 7:00Pm")
+
+// }else if(time == "2000"){
+//     document.write("time is 8:00Pm")
+
+// }else if(time == "2100"){
+//     document.write("time is 9:00Pm")
+
+// }else if(time == "2200"){
+//     document.write("time is 10:00Pm")
+
+// }else if(time == "2300"){
+//     document.write("time is 11:00Pm")
+
+// }else if(time == "0000"){
+//     document.write("time is 12:00Am")
+
+// }
+
+// var time = prompt("enter a time")
+
+//  if(time >= 100 && time < 1200){
+//     document.write("Good Morning")
+
+// }else if(time >= 1200 && time < 1700){
+//     document.write("Good Afternoon")
+
+// }else if(time >= 1700 && time < 2100){
+//     document.write("Good evening")
+
+// }

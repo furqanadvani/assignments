b1ced175bb38623f164c1d6b0442ca9089fc276f
furqanadvani <123248423+furqanadvani@users.noreply.github.com>
2023-02-28 06:07:45 -0800

    Add files via upload

---
+++ assigments/chapter5/aoo.js
@@ -0,0 +1,100 @@
+// Q1
+
+// var num1 = prompt("enter a number")
+// var num2 = prompt("enter a number")
+
+//  document.write("sum of " + num1 + " " + " and " + num2 + " is " + (Number(num1) + Number(num2)))
+
+// Q2
+
+// var anyNumber = prompt("enter any number")
+//  var anotherNumber = prompt("enter another number")
+//  document.write(`intial value is ${anyNumber} `)
+//  document.write(`after increament value is ${Number(++anyNumber)} `)
+// document.write(`after addition value is ${Number(anyNumber) + Number(anotherNumber)} `)
+// document.write(`after Subtraction value is ${Number(anyNumber) - Number(anotherNumber)} `)
+// document.write(`after multiplication value is ${Number(anyNumber) * Number(anotherNumber)} `)
+// document.write(`after division value is ${Number(anyNumber) / Number(anotherNumber)} `)
+// document.write(` and remainder value is ${Number(anyNumber) % Number(anotherNumber)} `)
+
+
+// Q4
+
+// var ticketCost = prompt("enter ticket cost")
+// var ticketqty = prompt("enter ticket Quantity")
+// document.write(`the total cost of buy ticket is ${Number(ticketCost) * Number(ticketqty)}PKR `)
+
+
+// Q5
+
+// var table = prompt("Enter any number")
+// document.write(` <h1>Table </h1>`)
+// document.write(`${table} x 1 = ${Number(table)*1} <br>`)
+// document.write(`${table} x 2 = ${Number(table)*2} <br>`)
+// document.write(`${table} x 3 = ${Number(table)*3} <br>`)
+// document.write(`${table} x 4 = ${Number(table)*4} <br>`)
+// document.write(`${table} x 5 = ${Number(table)*5} <br>`)
+// document.write(`${table} x 6 = ${Number(table)*6} <br>`)
+// document.write(`${table} x 7 = ${Number(table)*7} <br>`)
+// document.write(`${table} x 8 = ${Number(table)*8} <br>`)
+// document.write(`${table} x 9 = ${Number(table)*9} <br>`)
+// document.write(`${table} x 10 = ${Number(table)*10} <br>`)
+
+// Q6
+
+// var Temprature = prompt("enter any Temprature value")
+// document.write(`the celsius Temprature is C ${(Number(Temprature)-32)*5/9} <br>
+// the fahrenite Temprature is F ${(Number(Temprature)*9/5)+32}`)
+
+// Q7
+
+// var priceOfItem1 = prompt("enter price of item1")
+// var priceOfItem2 = prompt("enter price of item2")
+// var qtyOfItem1 = prompt("enter quantity of item2")
+// var qtyOfItem2 = prompt("enter quntity of item2")
+// var shippingPrice = prompt("shipping price")
+// document.write(`total cost of your is ${Number(priceOfItem1) * Number(qtyOfItem1) + Number(priceOfItem2 )*Number(qtyOfItem2)+Number(shippingPrice)}`)
+
+//  Q8
+// var totalMarks = prompt("enter total marks")
+// var obtainedMarks = prompt("enter obtained marks")
+// document.write(` <h1>Marks Sheet</h1> <br> total marks is ${totalMarks} <br> obtained marks is ${obtainedMarks} <br> percentage is ${Number(totalMarks)*Number(obtainedMarks)/100}`)
+
+
+// Q9
+
+// var dollar = prompt("Enter US Dollar")
+// var riyal = prompt("Enter Saudi Riyal")
+// document.write(`<h1> Currency In PKR </h1> <br>1 Dollar = 260PKR <br>
+// 1 Riyal = 72 <br> ${dollar} Dollar is ${Number(dollar)*260}PKR <br> ${riyal} Riyal is ${Number(riyal)*72}`)
+
+
+// Q10
+
+//  var $number = prompt("Enter a number")
+// document.write(`Caculation <br> ${$number} add 5 = ${Number($number)+5} <br>
+// ${$number} multiply by 10 = ${Number($number)*10} <br> ${$number} divide by 2 = ${Number($number)/2}`)
+
+// Q11
+
+// var currentyaer = prompt("Enter Current Year")
+// var birthyaer = prompt("Enter your birth year")
+
+// document.write (`<h1>Age Calculator </h1> <br>Current Year is ${currentyaer} <br>
+//  My Birth Year is ${birthyaer} <br> And My Age is ${Number(currentyaer)-Number(birthyaer)}`)
+
+// Q12
+
+// var radius = prompt("value of radius")
+// document.write(` <h1>The Geometrizer</h1> <br> the radius value is ${radius} <br> the circumference is ${2*3.142*Number(radius)}
+// <br> the Area is ${3.142*Number(radius)*2} `)
+
+// Q13
+// var snack = prompt("your favourite snacks")
+// var myAge = prompt("my Age")
+// var maxAge = prompt("Maximum Age")
+// var amountPerDay = prompt("Estimated amount per day you eat")
+
+// document.write(`<h1>Lifetime Supply Calculator</h1> <br> my favourite snacks is ${snack} <br> my age is ${myAge} <br> maximum age is ${maxAge} <br> Maximum amount i eat is ${amountPerDay} <br> ${Number(myAge)*Number(maxAge)*Number(amountPerDay)} `)
+
+// chapter 5 complete
